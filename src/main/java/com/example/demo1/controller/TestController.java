edccc0bf4596d73c993cf0d8a340b2a2c2723853
colin <zhangliang@zhangliang>
2018-12-22 09:37:00 +0800

    init

---
+++ src/main/java/com/example/demo1/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Auther: colin
+ * @Date: 2018/12/22 09:28
+ * @Description:
+ * @Version: V1.0
+ */
+@Controller
+public class TestController {
+
+
+    @RequestMapping(value = "/msg/{msg}",method = RequestMethod.GET)
+    @ResponseBody
+    public String getMsg(@PathVariable("msg")String msg){
+        return "the new msg is " + msg;
+    }
+}
