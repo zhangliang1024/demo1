d6643d01a74766d569370cf52ee3a5ff07e26644
colin <zhangliang@zhangliang>
2018-12-24 17:53:37 +0800

    add

--- src/main/java/com/example/demo1/controller/TestController.java
+++ src/main/java/com/example/demo1/controller/TestController.java
@@ -20,6 +20,6 @@ public class TestController {
     @ResponseBody
     public String getMsg(@PathVariable("msg")String msg){
         System.out.println("哈哈，再试一次");
-        return "the new msg is will be 呵呵 终于打通了 唉 不容易啊；今天很好高兴啊 哈哈 " + msg;
+        return "今天很好高兴啊 哈哈 " + msg;
     }
 }
